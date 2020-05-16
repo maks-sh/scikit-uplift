da664b63c67d85af557d77f79749ca841d269466
Maksim Shevchenko <maxik08@list.ru>
2020-05-16 23:15:58 +0300

    :lollipop: Fix logo background color and font-size of breadcrumbs

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = [
-    'custom.css',
+    'css/custom.css',
 ]
 html_js_files = ['https://buttons.github.io/buttons.js']
 html_logo = "./_static/sklift-logo.png"
