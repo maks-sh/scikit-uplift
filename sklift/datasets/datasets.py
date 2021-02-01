6de32b76a34cb9587aaf7b231a0fc646e3d65895
RobbStarkk <ruviktor@mail.ru>
2021-02-01 20:18:13 +0700

    reformated file to pep8 standart

--- sklift/datasets/datasets.py
+++ sklift/datasets/datasets.py
@@ -2,17 +2,22 @@ import os
 
 import requests
 
+
 def get_data_dir():
     pass
+
 
 def create_data_dir(path):
     pass
 
+
 def download(url, dest_path):
     pass
 
+
 def get_data(data_home, url, dest_subdir, dest_filename, download_if_missing):
     pass
+
 
 def clear_data_dir():
     pass
