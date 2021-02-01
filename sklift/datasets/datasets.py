3aac66b1c8948675c1db5b675a6a4d36882319f7
RobbStarkk <ruviktor@mail.ru>
2021-02-01 21:36:08 +0700

    Added clear_data_dir dockstring description

--- sklift/datasets/datasets.py
+++ sklift/datasets/datasets.py
@@ -20,5 +20,10 @@ def get_data(data_home, url, dest_subdir, dest_filename, download_if_missing):
 
 
 def clear_data_dir(path):
+    """This function deletes the file.
+
+        Args:
+            path (str): File path.
+        """
     if os.path.isfile(path):
         os.remove(path)
