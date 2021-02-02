320e1712867bbb4dbdebb50aa2720da9e84f2fb1
RobbStarkk <ruviktor@mail.ru>
2021-02-02 18:36:44 +0700

    Rebuild clear_data_dir, now it remove directory with ds and default path is get_data_dir

--- sklift/datasets/datasets.py
+++ sklift/datasets/datasets.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import requests
 
@@ -19,11 +20,13 @@ def get_data(data_home, url, dest_subdir, dest_filename, download_if_missing):
     pass
 
 
-def clear_data_dir(path):
+def clear_data_dir(path=None):
     """This function deletes the file.
 
         Args:
-            path (str): File path.
+            path (str): File path. By default, this is the default path for datasets.
         """
-    if os.path.isfile(path):
-        os.remove(path)
+    if path is None:
+        path = get_data_dir()
+    if os.path.isdir(path):
+        shutil.rmtree(path, ignore_errors=True)
