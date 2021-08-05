6d6bcfa03f6e7a42acd69e8b043ddf6847fbd6a5
Maksim Shevchenko <maxik08@list.ru>
2021-08-06 02:20:38 +0300

    :hammer: Fix code style in tests

--- sklift/tests/test_plot_qini_curve.py
+++ sklift/tests/test_plot_qini_curve.py
@@ -10,21 +10,24 @@ from ..viz import UpliftCurveDisplay
 from sklearn.tree import DecisionTreeClassifier
 from ..models import SoloModel
 
-@pytest.mark.parametrize("random", [True,False])
-@pytest.mark.parametrize("perfect", [True,False])
-@pytest.mark.parametrize("negative_effect", [True,False])
-def test_plot_qini_curve(random, perfect, negative_effect):
 
-    X_train, y_train, treat_train = np.array([[5.1, 3.5, 1.4, 0.2],[4.9, 3.0, 1.4, 0.2],[4.7, 3.2, 1.3, 0.2]]), np.array([0.0,0.0,1.0]), np.array([0.0,1.0,1.0])
-    X_val, y_val, treat_val = np.array([[5.1, 3.4, 1.5, 0.2],[5.0, 3.5, 1.3, 0.3],[4.5, 2.3, 1.3, 0.3]]), np.array([0.0,1.0,0.0]), np.array([0.0,1.0,1.0])
+@pytest.mark.parametrize("random", [True, False])
+@pytest.mark.parametrize("perfect", [True, False])
+@pytest.mark.parametrize("negative_effect", [True, False])
+def test_plot_qini_curve(random, perfect, negative_effect):
+    X_train, y_train, treat_train = np.array(
+        [[5.1, 3.5, 1.4, 0.2], [4.9, 3.0, 1.4, 0.2], [4.7, 3.2, 1.3, 0.2]]), np.array([0.0, 0.0, 1.0]), np.array(
+        [0.0, 1.0, 1.0])
+    X_val, y_val, treat_val = np.array([[5.1, 3.4, 1.5, 0.2], [5.0, 3.5, 1.3, 0.3], [4.5, 2.3, 1.3, 0.3]]), np.array(
+        [0.0, 1.0, 0.0]), np.array([0.0, 1.0, 1.0])
 
     model = DecisionTreeClassifier()
 
-    tm = SoloModel(model)
+    s_model = SoloModel(model)
 
-    tm = tm.fit(X_train, y_train, treat_train)
+    s_model = s_model.fit(X_train, y_train, treat_train)
 
-    uplift_preds = tm.predict(X_val)
+    uplift_preds = s_model.predict(X_val)
 
     y_true, uplift, treatment = y_val, uplift_preds, treat_val
 
@@ -39,11 +42,11 @@ def test_plot_qini_curve(random, perfect, negative_effect):
         x_baseline, y_baseline = x_actual, x_actual * y_actual[-1] / len(y_true)
         assert_allclose(viz.x_baseline, x_baseline)
         assert_allclose(viz.y_baseline, y_baseline)
-    
+
     if perfect:
         x_perfect, y_perfect = perfect_qini_curve(
             y_true, treatment, negative_effect)
-        
+
         assert_allclose(viz.x_perfect, x_perfect)
         assert_allclose(viz.y_perfect, y_perfect)
 
@@ -62,13 +65,13 @@ def test_plot_qini_curve(random, perfect, negative_effect):
     ]
 )
 def test_default_labels(qini_auc, estimator_name, expected_label):
-    x_actual = np.array([0,1,2,3,5,6])
-    y_actual = np.array([0.0,1.0,2.0,3.0,2.5,1.5])
+    x_actual = np.array([0, 1, 2, 3, 5, 6])
+    y_actual = np.array([0.0, 1.0, 2.0, 3.0, 2.5, 1.5])
 
     disp = UpliftCurveDisplay(
-        x_actual= x_actual,
+        x_actual=x_actual,
         y_actual=y_actual,
-        estimator_name = estimator_name
+        estimator_name=estimator_name
     ).plot(qini_auc, title="plot_qini_curve")
 
     assert disp.line_.get_label() == expected_label
--- sklift/tests/test_plot_uplift_curve.py
+++ sklift/tests/test_plot_uplift_curve.py
@@ -10,21 +10,22 @@ from ..viz import UpliftCurveDisplay
 from sklearn.tree import DecisionTreeClassifier
 from ..models import SoloModel
 
-@pytest.mark.parametrize("random", [True,False])
-@pytest.mark.parametrize("perfect", [True,False])
-def test_plot_uplift_curve(random, perfect):
 
-    X_train, y_train, treat_train = (np.array([[5.1, 3.5, 1.4, 0.2],[4.9, 3.0, 1.4, 0.2],[4.7, 3.2, 1.3, 0.2]]),
-                                        np.array([0.0,0.0,1.0]), np.array([0.0,1.0,1.0]))
-    X_val, y_val, treat_val = np.array([[5.1, 3.4, 1.5, 0.2],[5.0, 3.5, 1.3, 0.3],[4.5, 2.3, 1.3, 0.3]]), np.array([0.0,1.0,0.0]), np.array([0.0,1.0,1.0])
+@pytest.mark.parametrize("random", [True, False])
+@pytest.mark.parametrize("perfect", [True, False])
+def test_plot_uplift_curve(random, perfect):
+    X_train, y_train, treat_train = (np.array([[5.1, 3.5, 1.4, 0.2], [4.9, 3.0, 1.4, 0.2], [4.7, 3.2, 1.3, 0.2]]),
+                                     np.array([0.0, 0.0, 1.0]), np.array([0.0, 1.0, 1.0]))
+    X_val, y_val, treat_val = np.array([[5.1, 3.4, 1.5, 0.2], [5.0, 3.5, 1.3, 0.3], [4.5, 2.3, 1.3, 0.3]]), np.array(
+        [0.0, 1.0, 0.0]), np.array([0.0, 1.0, 1.0])
 
     model = DecisionTreeClassifier()
 
-    tm = SoloModel(model)
+    s_model = SoloModel(model)
 
-    tm = tm.fit(X_train, y_train, treat_train)
+    s_model = s_model.fit(X_train, y_train, treat_train)
 
-    uplift_preds = tm.predict(X_val)
+    uplift_preds = s_model.predict(X_val)
 
     y_true, uplift, treatment = y_val, uplift_preds, treat_val
 
@@ -39,11 +40,11 @@ def test_plot_uplift_curve(random, perfect):
         x_baseline, y_baseline = x_actual, x_actual * y_actual[-1] / len(y_true)
         assert_allclose(viz.x_baseline, x_baseline)
         assert_allclose(viz.y_baseline, y_baseline)
-    
+
     if perfect:
         x_perfect, y_perfect = perfect_uplift_curve(
             y_true, treatment)
-        
+
         assert_allclose(viz.x_perfect, x_perfect)
         assert_allclose(viz.y_perfect, y_perfect)
 
@@ -62,13 +63,13 @@ def test_plot_uplift_curve(random, perfect):
     ]
 )
 def test_default_labels(uplift_auc, estimator_name, expected_label):
-    x_actual = np.array([0,1,2,3,5,6])
-    y_actual = np.array([0.0,1.0,2.0,3.0,2.5,1.5])
+    x_actual = np.array([0, 1, 2, 3, 5, 6])
+    y_actual = np.array([0.0, 1.0, 2.0, 3.0, 2.5, 1.5])
 
     disp = UpliftCurveDisplay(
-        x_actual= x_actual,
+        x_actual=x_actual,
         y_actual=y_actual,
-        estimator_name = estimator_name
+        estimator_name=estimator_name
     ).plot(uplift_auc, title="plot_uplift_curve")
 
     assert disp.line_.get_label() == expected_label
